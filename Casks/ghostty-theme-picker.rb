772943418cc7d1889ae92e8a43465d44cc3a7f00
Chris Fields <chfields@chriss.macbook.pro.2.lan>
2026-02-05 03:17:55 -0600

    Fix ghostty-theme-picker v1.2.0 DMG checksum

--- Casks/ghostty-theme-picker.rb
+++ Casks/ghostty-theme-picker.rb
@@ -1,6 +1,6 @@
 cask "ghostty-theme-picker" do
   version "1.2.0"
-  sha256 "3279a2d02899184647b438d4c4788a5f0182b632ec5bbe0af1791f55f4d962cc"
+  sha256 "2f05110e1d5988c18e71452ae3379a9d4a5d24b52337cff7f5babe0432e6e333"
 
   url "https://github.com/chfields/GhosttyThemePicker/releases/download/v#{version}/GhosttyThemePicker.dmg"
   name "Ghostty Theme Picker"
